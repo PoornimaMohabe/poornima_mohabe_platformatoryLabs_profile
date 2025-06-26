1ec1c8713d1a767aeff550bd706d888c12090fdd
PoornimaMohabe <mohbepurnima@gmail.com>
2025-06-26 18:28:48 +0530

    added vercel json file

--- frontend/profile-app/src/pages/EditProfilePage.jsx
+++ frontend/profile-app/src/pages/EditProfilePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { BASE_URL } from "../../utils/url";
 
 const EditProfilePage = () => {
   const { id: userId } = useParams();
